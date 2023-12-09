2ecbbb464315464ec408b3055d3982b81560c1db
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-10 09:45:48 +1300

    Added conversations endpoint

--- app/backend/fraude/app.py
+++ app/backend/fraude/app.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 
+from fraude.db import DbClient
+from fraude.constants import DB_URL, DB_NAME
+from fraude.models import ConversationHeaders
+
 
 app = FastAPI()
+
+# todo inject db_client
+db_client = DbClient(DB_URL, DB_NAME)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/conversations")
+async def get_conversations() -> ConversationHeaders:
+    user_id = "FAKE_USER"
+    return db_client.get_conversation_headers(user_id)
 
 
 if __name__ == "__main__":
