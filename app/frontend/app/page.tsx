72e7f06306ad2d9f80d10cc1d864080a94c02e0f
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-16 10:20:25 +1300

    Added title

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <div id='header'>
-
+        <h1><i>Fr</i>aude</h1>
       </div>
       <div id='main'><ChatApp></ChatApp></div>
       <div id='footer'></div>
