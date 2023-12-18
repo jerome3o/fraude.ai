f1b637caf7336333ff0067e606c206cc313ea5df
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-19 12:00:26 +1300

    Added script running in fe

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,8 +1,12 @@
 import ChatApp from '../components/ChatApp'
+import Script from 'next/script'
+
+// TODO: need to double check how the Script should be used
 
 export default function Home() {
   return (
     <main>
+      <Script src="https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js" />
       <div id='header'>
         <h1><i>Fr</i>aude</h1>
       </div>
--- app/frontend/fraude/apiService.ts
+++ app/frontend/fraude/apiService.ts
@@ -1,3 +1,21 @@
+function prepPythonCode(python: string) {
+  return `
+globals().clear()
+from io import StringIO
+import sys
+
+_old_stdout = sys.stdout
+sys.stdout = StringIO()
+
+${python}
+
+sys.stdout.seek(0)
+output = sys.stdout.read()
+sys.stdout = _old_stdout
+output
+`;
+}
+
 
 // in python it's list[tuple[str, str]]
 type ConversationHeaders = {
@@ -43,6 +61,27 @@ function getThread(messages: StoredMessage[]): StoredMessage[] {
   return thread;
 }
 
+let pyodide: any;
+
+const runScript = async (code: string) => {
+  if (!pyodide) {
+    console.log("loading pyodide")
+    pyodide = await window.loadPyodide({
+      indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+    });
+    console.log("loading pyodide complete");
+  }
+
+  console.log(code);
+
+  try {
+    return await pyodide.runPythonAsync(prepPythonCode(code));
+  } catch (error) {
+    console.error(error);
+    return error.message;
+  }
+}
+
 
 class ApiService {
   private baseUrl: string;
@@ -52,6 +91,9 @@ class ApiService {
   // default to ""
   constructor(user: string, baseUrl: string = "") {
     this.user = user;
+
+    // learn where/when code is executed in next/react. This gives errors on
+    // compile time
     this.baseUrl = baseUrl || (window && window.location.origin);
 
     // handle when http is in the URL.. ?
@@ -114,15 +156,26 @@ class ApiService {
       if (data.type === "partial_response") {
         partialMessage += data.content
         onPartial(partialMessage);
+        return;
       }
 
       if (data.type === "execute_code") {
-        socket.send(JSON.stringify({
-          type: "execute_code_response",
-          stdout: "Error, failed to run the python interpreter",
-          files: [],
-          exit_code: 1
-        }));
+        // socket.send(JSON.stringify({
+        //   type: "execute_code_response",
+        //   stdout: "Error, failed to run the python interpreter",
+        //   files: [],
+        //   exit_code: 1
+        // }));
+        runScript(data.content).then((result) => {
+          console.log(result);
+          socket.send(JSON.stringify({
+            type: "execute_code_response",
+            stdout: result,
+            files: [],
+            exit_code: 0
+          }));
+        })
+        return;
       }
 
       console.error(`Not implemented: ${data.type}`);
