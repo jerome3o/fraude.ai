4eee3afe7da6d2cb9cd5c6824ccaec541f0e316f
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-26 16:29:43 +1300

    Fixed dep issues, updated docker-compose, fixed debug config

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -1,5 +1,5 @@
-import ChatApp from '../components/ChatApp'
-import Script from 'next/script'
+import ChatApp from "../components/ChatApp";
+import Script from "next/script";
 
 // TODO: need to double check how the Script should be used
 
@@ -7,11 +7,15 @@ export default function Home() {
   return (
     <main>
       <Script src="https://cdn.jsdelivr.net/pyodide/v0.18.1/full/pyodide.js" />
-      <div id='header'>
-        <h1><i>Fr</i>aude</h1>
+      <div id="header">
+        <h1>
+          <i>Fr</i>aude
+        </h1>
       </div>
-      <div id='main'><ChatApp></ChatApp></div>
-      <div id='footer'></div>
+      <div id="main">
+        <ChatApp></ChatApp>
+      </div>
+      <div id="footer"></div>
     </main>
-  )
+  );
 }
